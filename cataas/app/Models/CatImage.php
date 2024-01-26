dd476f1506067152bc8c86e2077f12be218600c6
danaguzman <danalozano0612@gmail.com>
2024-01-26 16:44:27 +0100

    Models clase CatImage

--- cataas/app/Models/CatImage.php
+++ cataas/app/Models/CatImage.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class CatImage extends Model
 {
-    use HasFactory;
+    protected $table = 'cat_images'; // Nom de la taula a la base de dades
+    protected $fillable = ['_id', 'mimetype', 'size', 'tags']; // Camps que es poden omplir
+
+    protected $casts = [
+        'tags' => 'json', // Especifica que el camp 'tags' és de tipus JSON
+    ];
 }
