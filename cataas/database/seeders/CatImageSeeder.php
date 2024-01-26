89b36eff5dd423ab0135a3c44f28eba73ae2cc2c
danaguzman <danalozano0612@gmail.com>
2024-01-26 16:45:09 +0100

    Modificacion seeder

--- cataas/database/seeders/CatImageSeeder.php
+++ cataas/database/seeders/CatImageSeeder.php
@@ -12,6 +12,18 @@ class CatImageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $client = new Client(); //  GuzzleHttp\Client class
+
+        $response = $client->get('https://cataas.com/api/cats?&skip=0&limit=100000');
+        $data = json_decode($response->getBody(), true);
+
+        foreach ($data as $item) {
+            CatImage::create([
+                '_id' => $item['_id'],
+                'mimetype' => $item['mimetype'],
+                'size' => isset($item['size']) ? $item['size'] : '0',
+                'tags' => json_encode($item['tags']),
+            ]);
+        }
     }
 }
