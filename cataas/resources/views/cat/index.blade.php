7ddb95dd0375c8057cd07f83dd2544a6be81cb72
danaguzman <danalozano0612@gmail.com>
2024-01-26 17:07:20 +0100

    modificando vista

--- cataas/resources/views/cat/index.blade.php
+++ cataas/resources/views/cat/index.blade.php
@@ -1,3 +1,9 @@
 <div>
-    <!-- When there is no desire, all things are at peace. - Laozi -->
+<h1>Lista de imágenes de gatos</h1>
+
+@foreach ($catImages as $catImage)
+    {{ $catImage->_id }}
+    <!-- Mostrar otros campos según sea necesario -->
+@endforeach
+
 </div>
